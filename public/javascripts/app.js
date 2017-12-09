039d0fed7bfc11c6be56c78461e63719d5566ef9
Lingzhi Zhang <zhangl@union.edu>
2017-12-08 23:36:49 -0500

    new UI!!!!!

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -24,19 +24,14 @@ app.controller('myController', function($scope, $http) {
         $scope.Top10 = function() {
 
         var request1 = $http.get('/Top10');
-        // $scope.loading = true;
-        // $scope.posts = "Posts".query(function ()  {
-        //     $scope.loading = false;
-        // });
         request1.success(function(data) {
+            
             $scope.data = data;
         });
         request1.error(function(data){
             console.log('err');
         });
     }; 
-
-
 
         $scope.Top50 = function() {
 
@@ -63,26 +58,67 @@ app.controller('myController', function($scope, $http) {
         });
         }; 
 
+});
 
+
+app.controller('genreController', function($scope, $http) {
+        $scope.message="";
+
+        $scope.Submit = function() {
+    
+        var request = $http.get('/genreshowall');
+        
+        request.success(function(data) {
+            
+            $scope.data = data;
+            // console.log('test controller');
+        });
+        request.error(function(data){
+            console.log('err');
+        });
+        };
+
+
+        $scope.Top5 = function() {
+
+        var request1 = $http.get('/genreTop5');
+        request1.success(function(data) {
+            
+            $scope.data = data;
+        });
+        request1.error(function(data){
+            console.log('err');
+        });
+    }; 
+
+        $scope.Top20 = function() {
+
+        var request1 = $http.get('/genreTop20');
+        request1.success(function(data) {
+
+            $scope.data = data;
+        });
+        request1.error(function(data){
+            console.log('err');
+        });
+        }; 
+
+
+        $scope.Empty = function() {
+
+        var request1 = $http.get('/Empty');
+        request1.success(function(data) {
+
+            $scope.data = data;
+        });
+        request1.error(function(data){
+            console.log('err');
+        });
+        }; 
 
 });
 
 
-// app.controller('myController2', function($scope, $http) {
-//         $scope.Top10 = function() {
-
-//         console.log('test controller')
-//         var request = $http.get('/data');
-//         request.success(function(data) {
-            
-//             $scope.data = data;
-//         });
-//         request.error(function(data){
-//             console.log('err');
-//         };
-    
-//     }; 
-// });
 
 
 // app.controller('insertController', function($scope, $http) {
--- routes/index.js
+++ routes/index.js
@@ -56,7 +56,7 @@ router.get('/showall', function(req,res) {
   //var email = req.params.email;
   //if (email != 'undefined') query = query + ' where login ="' + email + '"' ;
   // console.log(query);
-  // console.log('test controller');
+  console.log('===============test controller==============');
   connection.query(query, function(err, rows, fields) {
     if (err) console.log(err);
     else {
@@ -84,11 +84,11 @@ router.get('/Top10', function(req,res) {
 
   var query = "SELECT * FROM user LIMIT 10"
   console.log(query);
-  // console.log('test!!!!!!!!!!!!!!!!!!!!!!!!!!')
+  console.log('test!!!!!!!!!!!!!!!!!!!!!!!!!!')
 
   connection.query(query, function(err, rows, fields) {
     if (err) console.log(err);
-    	//return callback(err, null);
+      //return callback(err, null);
     else {
         res.json(rows);
         //return callback(null, rows);
@@ -96,10 +96,12 @@ router.get('/Top10', function(req,res) {
     });
 });
 
+
+
 router.get('/Top50', function(req,res) {
   // use console.log() as print() in case you want to debug, example below:
   // console.log("inside person email");
-  var query = 'SELECT * from Person Limit 3';
+  var query = 'SELECT * from animation Limit 3';
 
   console.log(query);
   // console.log('test!!!!!!!!!!!!!!!!!!!!!!!!!!')
@@ -127,6 +129,77 @@ router.get('/Empty', function(req,res) {
     }  
     });
 });
+
+
+
+router.get('/genreshowall', function(req,res) {
+  // use console.log() as print() in case you want to debug, example below:
+  // console.log("inside person email");
+  var query = 'SELECT g.genre, AVG(a.score)';
+  query += "FROM animation a, animation_genre ag, genre g"
+  query += " WHERE a.anime_id = ag.anime_id AND ag.genre_label = g.genre_label"
+  query += " GROUP BY g.genre"
+  query += " ORDER BY AVG(a.score) DESC"
+  console.log(query)
+  //var email = req.params.email;
+  //if (email != 'undefined') query = query + ' where login ="' + email + '"' ;
+  // console.log(query);
+  // console.log('test controller');
+  connection.query(query, function(err, rows, fields) {
+    if (err) console.log(err);
+    else {
+        res.json(rows);
+    }  
+    });
+});
+
+
+
+router.get('/genreTop5', function(req,res) {
+  // use console.log() as print() in case you want to debug, example below:
+  // console.log("inside person email");
+  var query = 'SELECT g.genre, AVG(a.score)';
+  query += "FROM animation a, animation_genre ag, genre g"
+  query += " WHERE a.anime_id = ag.anime_id AND ag.genre_label = g.genre_label"
+  query += " GROUP BY g.genre"
+  query += " ORDER BY AVG(a.score) DESC LIMIT 5"
+  console.log(query)
+  //var email = req.params.email;
+  //if (email != 'undefined') query = query + ' where login ="' + email + '"' ;
+  // console.log(query);
+  // console.log('test controller');
+  connection.query(query, function(err, rows, fields) {
+    if (err) console.log(err);
+    else {
+        res.json(rows);
+    }  
+    });
+});
+
+
+
+router.get('/genreTop20', function(req,res) {
+  // use console.log() as print() in case you want to debug, example below:
+  // console.log("inside person email");
+  var query = 'SELECT g.genre, AVG(a.score)';
+  query += "FROM animation a, animation_genre ag, genre g"
+  query += " WHERE a.anime_id = ag.anime_id AND ag.genre_label = g.genre_label"
+  query += " GROUP BY g.genre"
+  query += " ORDER BY AVG(a.score) DESC LIMIT 20"
+  console.log(query)
+  //var email = req.params.email;
+  //if (email != 'undefined') query = query + ' where login ="' + email + '"' ;
+  // console.log(query);
+  // console.log('test controller');
+  connection.query(query, function(err, rows, fields) {
+    if (err) console.log(err);
+    else {
+        res.json(rows);
+    }  
+    });
+});
+
+
 
 
 
